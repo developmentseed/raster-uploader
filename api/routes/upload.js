cc1a757e80d1cf9ed75886eb3ffe9c86e9fda37d
ingalls <nick@ingalls.ca>
2022-05-24 12:36:05 -0600

    Add upload route

---
+++ api/routes/upload.js
@@ -0,0 +1,107 @@
+import { Err } from '@openaddresses/batch-schema';
+import Upload from '../lib/upload.js';
+import Auth from '../lib/auth.js';
+
+export default async function router(schema, config) {
+    /**
+     * @api {get} /api/upload List Uploads
+     * @apiVersion 1.0.0
+     * @apiName ListUploads
+     * @apiGroup Upload
+     * @apiPermission user
+     *
+     * @apiDescription
+     *     Return a list of uploads that have been attempted by a user
+     *
+     * @apiSchema (Query) {jsonschema=../schema/req.query.ListUploads.json} apiParam
+     * @apiSchema {jsonschema=../schema/res.ListUploads.json} apiSuccess
+     */
+    await schema.get('/upload', {
+        query: 'req.query.ListUploads.json',
+        res: 'res.ListUploads.json'
+    }, async (req, res) => {
+        try {
+            await Auth.is_auth(req);
+
+            const list = await Upload.list(config.pool, req.query);
+
+            res.json(list);
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+
+    /**
+     * @api {post} /api/user Create Upload
+     * @apiVersion 1.0.0
+     * @apiName CreateUpload
+     * @apiGroup Upload
+     t @apiPermission user
+     *
+     * @apiDescription
+     *     Create a new upload
+     *
+     * @apiSchema {jsonschema=../schema/res.Upload.json} apiSuccess
+     */
+    await schema.post('/user', {
+        res: 'res.User.json'
+    }, async (req, res) => {
+        try {
+            await Auth.is_auth(req);
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+
+    /**
+     * @api {patch} /api/upload/:upload Update Upload
+     * @apiVersion 1.0.0
+     * @apiName PatchUpload
+     * @apiGroup User
+     * @apiPermission user
+     *
+     * @apiDescription
+     *     Update information about a given upload
+     *
+     * @apiParam {Number} :upload The ID of the upload
+     *
+     * @apiSchema (Body) {jsonschema=../schema/req.body.PatchUpload.json} apiParam
+     * @apiSchema {jsonschema=../schema/res.Upload.json} apiSuccess
+     */
+    await schema.patch('/upload/:upload', {
+        ':upload': 'integer',
+        body: 'req.body.PatchUpload.json',
+        res: 'res.Upload.json'
+    }, async (req, res) => {
+        try {
+            await Auth.is_auth(req);
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+
+    /**
+     * @api {delete} /api/upload/:upload Delete Upload
+     * @apiVersion 1.0.0
+     * @apiName DeleteUpload
+     * @apiGroup User
+     * @apiPermission user
+     *
+     * @apiDescription
+     *     Delete a given upload
+     *
+     * @apiParam {Number} :upload The ID of the upload
+     *
+     * @apiSchema {jsonschema=../schema/res.Standard.json} apiSuccess
+     */
+    await schema.delete('/upload/:upload', {
+        ':upload': 'integer',
+        res: 'res.Standard.json'
+    }, async (req, res) => {
+        try {
+            await Auth.is_auth(req);
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+}
