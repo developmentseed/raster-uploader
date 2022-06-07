cd10073e94c46635fb06cc4e6f8d8f2c7bebdd9c
ingalls <nick@ingalls.ca>
2022-06-06 21:32:10 -0600

    Upload list page

--- api/web/src/components/Home.vue
+++ api/web/src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class='col col--12'>
         <div class='col col--12 clearfix py6'>
-            <h2 class='fl cursor-default'>Projects</h2>
+            <h2 class='fl cursor-default'>Uploads</h2>
 
             <div class='fr'>
                 <label class='switch-container px6'>
@@ -16,7 +16,7 @@
                 <button @click='refresh' class='btn round btn--stroke color-gray color-blue-on-hover mr12'>
                     <svg class='icon'><use href='#icon-refresh'/></svg>
                 </button>
-                <button @click='$router.push({ name: "newproject" })' class='btn round btn--stroke color-gray color-green-on-hover'>
+                <button @click='$router.push({ name: "newupload" })' class='btn round btn--stroke color-gray color-green-on-hover'>
                     <svg class='icon'><use href='#icon-plus'/></svg>
                 </button>
             </div>
@@ -28,50 +28,39 @@
                         <div class='absolute flex flex--center-cross flex--center-main w36 h36'>
                             <svg class='icon'><use xlink:href='#icon-search'></use></svg>
                         </div>
-                        <input ref='search' v-model='search' class='input pl36' placeholder='Model Name'>
+                        <input ref='search' v-model='search' class='input pl36' placeholder='Upload Name'>
                     </div>
                 </div>
             </template>
-            <template v-if='loading.projects'>
-                <Loading desc='Loading Projects'/>
+            <template v-if='loading.uploads'>
+                <Loading desc='Loading Uploads'/>
             </template>
-            <template v-else-if='projects.length === 0'>
+            <template v-else-if='uploads.length === 0'>
                 <div class='flex flex--center-main pt36'>
                     <svg class='flex-child icon w60 h60 color--gray'><use href='#icon-info'/></svg>
                 </div>
 
                 <div class='flex flex--center-main pt12 pb36'>
-                    <h1 class='flex-child txt-h4 cursor-default'>No Projects Found</h1>
+                    <h1 class='flex-child txt-h4 cursor-default'>No Uploads Found</h1>
                 </div>
             </template>
             <template v-else>
-                <div @click='$router.push({ name: "project", params: { projectid: project.id } })' :key='project.modelId' v-for='project in projects.slice(page * 10, page * 10 + 10)'>
+                <div @click='$router.push({ name: "upload", params: { uploadid: upload.id } })' :key='upload.id' v-for='upload in uploads.slice(page * 10, page * 10 + 10)'>
                     <div class='cursor-pointer bg-darken10-on-hover col col--12 py12'>
                         <div class='col col--12 grid py6 px12'>
                             <div class='col col--6'>
                                 <div class='col col--12 clearfix'>
-                                    <svg v-if='project.access === "private"' class='fl icon color-gray h24 w24'><use xlink:href='#icon-lock'/></svg>
+                                    <svg v-if='upload.access === "private"' class='fl icon color-gray h24 w24'><use xlink:href='#icon-lock'/></svg>
 
-                                    <h3 class='txt-h4 fl' v-text='project.name'></h3>
-                                    <svg @click.prevent.stop='$router.push({ name: "editproject", params: { projectid: project.id } })' class='fl my6 mx6 icon cursor-pointer color-gray-light color-gray-on-hover'><use href='#icon-pencil'/></svg>
+                                    <h3 class='txt-h4 fl' v-text='upload.name'></h3>
+                                    <svg @click.prevent.stop='$router.push({ name: "editupload", params: { uploadid: upload.id } })' class='fl my6 mx6 icon cursor-pointer color-gray-light color-gray-on-hover'><use href='#icon-pencil'/></svg>
                                 </div>
                                 <div class='col col--12'>
-                                    <h3 class='txt-xs' v-text='project.source'></h3>
+                                    <h3 class='txt-xs' v-text='upload.source'></h3>
                                 </div>
                             </div>
                             <div class='col col--6'>
-                                <div v-if='project.project_url' @click.prevent.stop='external(project.project_url)' class='fr bg-blue-faint bg-blue-on-hover color-white-on-hover color-blue inline-block px6 py3 round txt-xs txt-bold cursor-pointer mr3'>
-                                    Project Page
-                                </div>
-
-                                <div v-if='project.repo' @click.prevent.stop='external(project.repo)' class='fr bg-blue-faint bg-blue-on-hover color-white-on-hover color-blue inline-block px6 py3 round txt-xs txt-bold cursor-pointer mr3'>
-                                    <svg class='icon' style='height: 15px; width: 15px;'><use xlink:href='#icon-github'/></svg>
-                                </div>
-
-                                <div v-if='project.stacks.length' class='fr bg-green-faint bg-green-on-hover color-white-on-hover color-green inline-block px6 py3 round txt-xs txt-bold mr3'>
-                                    Active Stack
-                                </div>
-                                <div v-if='project.archived' class='fr bg-gray-faint bg-gray-on-hover color-white-on-hover color-gray inline-block px6 py3 round txt-xs txt-bold mr3'>
+                                <div v-if='upload.archived' class='fr bg-gray-faint bg-gray-on-hover color-white-on-hover color-gray inline-block px6 py3 round txt-xs txt-bold mr3'>
                                     Archived
                                 </div>
                             </div>
@@ -81,7 +70,7 @@
 
                 <Pager
                     @page='page = $event'
-                    :total='projects.length'
+                    :total='uploads.length'
                     perpage='10'
                 />
             </template>
@@ -102,9 +91,9 @@ export default {
             showSearch: false,
             search: '',
             archived: false,
-            projects: [],
+            uploads: [],
             loading: {
-                projects: true
+                uploads: true
             }
         }
     },
@@ -140,24 +129,24 @@ export default {
     methods: {
         refresh: function() {
             this.page = 0;
-            this.getProjects();
+            this.getUploads();
         },
         external: function(url) {
             if (!url) return;
             window.open(url, "_blank")
         },
-        getProjects: async function() {
-            this.loading.projects = true;
-            this.projects = [];
+        getUploads: async function() {
+            this.loading.uploads = true;
+            this.uploads = [];
 
             try {
-                const url = new URL('/api/project', window.api);
+                const url = new URL('/api/upload', window.api);
                 url.searchParams.append('filter', this.search);
                 url.searchParams.append('archived', this.archived);
-                const projects = await window.std(url);
+                const uploads = await window.std(url);
 
-                this.projects = projects.projects;
-                this.loading.projects = false;
+                this.uploads = uploads.uploads;
+                this.loading.uploads = false;
             } catch (err) {
                 this.$emit('err', err);
             }
