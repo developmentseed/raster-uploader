b23effd0ecdc04f6802b245dbdca0b04ecd19dca
ingalls <nick@ingalls.ca>
2022-06-21 09:19:29 -0600

    Add config UI sketch & config col to uploads table

--- api/web/src/components/Upload.vue
+++ api/web/src/components/Upload.vue
@@ -7,7 +7,39 @@
                 <span>New Upload</span>
             </h2>
         </div>
-        <div class='round mb60 col col--12'>
+        <div class='round mb60 col col--12 grid border border--gray-light py12 px12'>
+            <div class='col col--12 grid grid--gut12 mb12'>
+                <div class='col col--12'>
+                    <h2>COG Output</h2>
+                </div>
+
+                <div class='col col--4'>
+                    <label>Block Size</label>
+                    <span class='fr ml3 cursor-pointer color-gray-light color-black-on-hover' style='margin-top: 5px;'><svg class='icon'><use xlink:href='#icon-info'/></svg></span>
+                    <input type='number' v-model='defaults.blocksize' class='input'/>
+                </div>
+
+                <div class='col col--4'>
+                    <label>Compression</label>
+                    <span @click='external("")' class='fr ml3 cursor-pointer color-gray-light color-black-on-hover' style='margin-top: 5px;'><svg class='icon'><use xlink:href='#icon-info'/></svg></span>
+                    <div class='select-container w-full'>
+                        <select v-model='defaults.compression' class='select select--stroke'>
+                            <option>deflate</option>
+                            <option>jpeg</option>
+                            <option>webp</option>
+                            <option>zstd</option>
+                            <option>lzw</option>
+                            <option>lzma</option>
+                            <option>packbits</option>
+                            <option>lerc</option>
+                            <option>lerc_deflate</option>
+                            <option>raw</option>
+                        </select>
+                        <div class='select-arrow'></div>
+                    </div>
+                </div>
+            </div>
+
             <UploadFile
                 :single='true'
                 :url='base'
@@ -28,12 +60,19 @@ export default {
     data: function() {
         return {
             base: new URL('/api/upload', window.location.origin),
+            defaults: {
+                blocksize: 512,
+                compression: 'deflate'
+            },
             headers: {
                 Authorization: `Bearer ${localStorage.token}`
             }
         }
     },
     methods: {
+        external: function(url) {
+            window.open(url, '_blank');
+        }
     },
     components: {
         UploadFile
