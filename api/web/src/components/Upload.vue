a2052d94d35930ee15c31a2e1a50e64c51fcf4e6
ingalls <nick@ingalls.ca>
2022-06-21 09:47:40 -0600

    Add UploadSettings

--- api/web/src/components/Upload.vue
+++ api/web/src/components/Upload.vue
@@ -8,43 +8,7 @@
             </h2>
         </div>
         <div class='round mb60 col col--12 grid border border--gray-light py12 px12'>
-            <div class='col col--12 grid grid--gut12 mb12'>
-                <div class='col col--12'>
-                    <h2>COG Output</h2>
-                </div>
-
-                <div class='col col--4'>
-                    <label>Block Size</label>
-                    <span class='fr ml3 cursor-pointer color-gray-light color-black-on-hover' style='margin-top: 5px;'><svg class='icon'><use xlink:href='#icon-info'/></svg></span>
-                    <input type='number' v-model='defaults.blocksize' class='input'/>
-                </div>
-
-                <div class='col col--4'>
-                    <label>Compression</label>
-                    <span @click='external("")' class='fr ml3 cursor-pointer color-gray-light color-black-on-hover' style='margin-top: 5px;'><svg class='icon'><use xlink:href='#icon-info'/></svg></span>
-                    <div class='select-container w-full'>
-                        <select v-model='defaults.compression' class='select select--stroke'>
-                            <option>deflate</option>
-                            <option>jpeg</option>
-                            <option>webp</option>
-                            <option>zstd</option>
-                            <option>lzw</option>
-                            <option>lzma</option>
-                            <option>packbits</option>
-                            <option>lerc</option>
-                            <option>lerc_deflate</option>
-                            <option>raw</option>
-                        </select>
-                        <div class='select-arrow'></div>
-                    </div>
-                </div>
-
-                <div class='col col--4'>
-                    <label>Overview Level</label>
-                    <span class='fr ml3 cursor-pointer color-gray-light color-black-on-hover' style='margin-top: 5px;'><svg class='icon'><use xlink:href='#icon-info'/></svg></span>
-                    <input type='number' v-model='defaults.overview' class='input'/>
-                </div>
-            </div>
+            <UploadSettings/>
 
             <UploadFile
                 :single='true'
@@ -59,6 +23,7 @@
 
 <script>
 import UploadFile from './util/Upload.vue';
+import UploadSettings from './util/UploadSettings.vue';
 
 export default {
     name: 'Upload',
@@ -66,11 +31,6 @@ export default {
     data: function() {
         return {
             base: new URL('/api/upload', window.location.origin),
-            defaults: {
-                blocksize: 512,
-                compression: 'deflate',
-                overview: 5
-            },
             headers: {
                 Authorization: `Bearer ${localStorage.token}`
             }
@@ -82,6 +42,7 @@ export default {
         }
     },
     components: {
+        UploadSettings,
         UploadFile
     }
 }
---
+++ api/web/src/components/util/UploadSettings.vue
@@ -0,0 +1,61 @@
+<template>
+<div class='col col--12 grid mb12'>
+    <div class='col col--12'>
+        <h2>COG Output</h2>
+    </div>
+
+    <div class='col col--4 px6'>
+        <label>Block Size</label>
+        <span class='fr ml3 cursor-pointer color-gray-light color-black-on-hover' style='margin-top: 5px;'><svg class='icon'><use xlink:href='#icon-info'/></svg></span>
+        <input type='number' v-model='defaults.blocksize' class='input'/>
+    </div>
+
+    <div class='col col--4 px6'>
+        <label>Compression</label>
+        <span @click='external("")' class='fr ml3 cursor-pointer color-gray-light color-black-on-hover' style='margin-top: 5px;'><svg class='icon'><use xlink:href='#icon-info'/></svg></span>
+        <div class='select-container w-full'>
+            <select v-model='defaults.compression' class='select select--stroke'>
+                <option>deflate</option>
+                <option>jpeg</option>
+                <option>webp</option>
+                <option>zstd</option>
+                <option>lzw</option>
+                <option>lzma</option>
+                <option>packbits</option>
+                <option>lerc</option>
+                <option>lerc_deflate</option>
+                <option>raw</option>
+            </select>
+            <div class='select-arrow'></div>
+        </div>
+    </div>
+
+    <div class='col col--4 px6'>
+        <label>Overview Level</label>
+        <span class='fr ml3 cursor-pointer color-gray-light color-black-on-hover' style='margin-top: 5px;'><svg class='icon'><use xlink:href='#icon-info'/></svg></span>
+        <input type='number' v-model='defaults.overview' class='input'/>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'UploadSettings',
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data: function() {
+        return {
+            defaults: {
+                blocksize: 512,
+                compression: 'deflate',
+                overview: 5
+            },
+        }
+    },
+}
+
+</script>
