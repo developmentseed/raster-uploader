0f625ddeced626798216f89a6fccc8246e48db0b
ingalls <nick@ingalls.ca>
2022-06-24 08:29:50 -0600

    Add upload type

--- api/web/src/components/Upload.vue
+++ api/web/src/components/Upload.vue
@@ -12,14 +12,39 @@
                 @settings='settings = $event'
             />
 
-            <UploadFile
-                :single='true'
-                :url='base'
-                :headers='headers'
-                :fields='settings'
-                @err='$emit("err", $event)'
-                @ok='$router.push(`/upload/${$event.id}/`)'
-            />
+            <div class='col col--12 flex flex--center-main mb3'>
+                <div class='toggle-group mr18'>
+                    <label class='toggle-container'>
+                        <input v-model='uploadtype' id='upload' value='upload' checked name='upload-type' type='radio' />
+                        <div class='toggle toggle--s round'>Upload</div>
+                    </label>
+                    <label class='toggle-container'>
+                        <input v-model='uploadtype' id='http' value='http' name='upload-type' type='radio' />
+                        <div class='toggle toggle--s round'>HTTP</div>
+                    </label>
+                    <label class='toggle-container'>
+                        <input v-model='uploadtype' id='s3' value='s3' name='upload-type' type='radio' />
+                        <div class='toggle toggle--s round'>AWS S3</div>
+                    </label>
+                </div>
+            </div>
+
+            <template v-if='uploadtype === "upload"'>
+                <UploadFile
+                    :single='true'
+                    :url='base'
+                    :headers='headers'
+                    :fields='settings'
+                    @err='$emit("err", $event)'
+                    @ok='$router.push(`/upload/${$event.id}/`)'
+                />
+            </template>
+            <template v-else-if='uploadtype === "http"'>
+                HTTP
+            </template>
+            <template v-else-if='uploadtype === "s3"'>
+                AWS S3
+            </template>
         </div>
     </div>
 </template>
@@ -34,6 +59,7 @@ export default {
     data: function() {
         return {
             settings: {},
+            uploadtype: 'upload',
             base: new URL('/api/upload', window.location.origin),
             headers: {
                 Authorization: `Bearer ${localStorage.token}`
