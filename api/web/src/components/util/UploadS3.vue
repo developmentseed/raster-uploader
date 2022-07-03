d016f0abc361958682c60eecba2b961e6bca6230
ingalls <nick@ingalls.ca>
2022-07-03 13:59:30 -0600

    Fix S3 Schema

--- api/web/src/components/util/UploadS3.vue
+++ api/web/src/components/util/UploadS3.vue
@@ -69,8 +69,8 @@ export default {
                         obtain: {
                             type: 's3',
                             url: this.url,
-                            access_key_id: this.access_key_id,
-                            secret_access_key: this.secret_access_key,
+                            aws_access_key_id: this.access_key_id,
+                            aws_secret_access_key: this.secret_access_key,
                         }
                     }
                 });
