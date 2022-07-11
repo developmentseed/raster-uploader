bec8ce938cab32d43c6365b0774e357c80b82fb8
ingalls <nick@ingalls.ca>
2022-07-11 16:14:47 -0600

    Clean up Graph Init

--- api/web/src/components/steps/Graph.vue
+++ api/web/src/components/steps/Graph.vue
@@ -4,6 +4,13 @@
 </div>
 </template>
 
+<style>
+#cy {
+    height: 500px;
+    width: 100%;
+}
+</style>
+
 <script>
 import Cytoscape from 'cytoscape';
 
@@ -16,35 +23,54 @@ export default {
         return {
             graph: {},
             processed: {
-                elements: {
-                    nodes: [
-                        { data: { id: 'cat' } },
-                        { data: { id: 'bird' } },
-                        { data: { id: 'ladybug' } },
-                        { data: { id: 'aphid' } },
-                        { data: { id: 'rose' } },
-                        { data: { id: 'grasshopper' } },
-                        { data: { id: 'plant' } },
-                        { data: { id: 'wheat' } }
-                    ],
-                    edges: [
-                        { data: { source: 'cat', target: 'bird' } },
-                        { data: { source: 'bird', target: 'ladybug' } },
-                        { data: { source: 'bird', target: 'grasshopper' } },
-                        { data: { source: 'grasshopper', target: 'plant' } },
-                        { data: { source: 'grasshopper', target: 'wheat' } },
-                        { data: { source: 'ladybug', target: 'aphid' } },
-                        { data: { source: 'aphid', target: 'rose' } }
-                    ]
-                },
+                nodes: [],
+                edges: []
             }
         };
     },
     mounted: function() {
+        // Populate Nodes
+        this.processed.nodes.push({ data: { id: 'initial' } });
+        for (const step of this.steps.upload_steps) {
+            this.processed.nodes.push({ data: { id: String(step.id) } });
+        }
+
+        for (const step of this.steps.upload_steps) {
+            if (!step.parent) {
+                this.processed.edges.push({ data: {
+                    source: 'initial',
+                    target: String(step.id)
+                }});
+            } else {
+                this.processed.edges.push({ data: {
+                    source: String(step.parent),
+                    target: String(step.id)
+                }});
+            }
+        }
+
         this.$nextTick(() => {
-            this.graph = cytoscape({
+            this.graph = Cytoscape({
                 container: document.getElementById('cy'),
                 elements: this.processed,
+                boxSelectionEnabled: false,
+                autounselectify: true,
+                style: Cytoscape.stylesheet()
+                    .selector('node').css({
+                        'height': 80,
+                        'width': 80,
+                        'background-fit': 'cover',
+                        'border-color': '#000',
+                        'border-width': 3,
+                        'border-opacity': 0.5
+                    })
+                    .selector('edge').css({
+                        'curve-style': 'bezier',
+                        'width': 6,
+                        'target-arrow-shape': 'triangle',
+                        'line-color': '#ffaaaa',
+                        'target-arrow-color': '#ffaaaa'
+                    }),
                 layout: {
                     name: 'breadthfirst',
                     directed: true,
