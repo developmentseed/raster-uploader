b472a26ff7835fa19220457a7e2c33691a940ecb
ingalls <nick@ingalls.ca>
2022-08-02 17:28:16 -0400

    Add framework for multiple tests

--- tasks/obtain/handler.py
+++ tasks/obtain/handler.py
@@ -9,8 +9,8 @@ from io import BytesIO, SEEK_SET, SEEK_END
 def handler(event, context):
     event = json.loads(event["Records"][0]["body"])
 
-    if event["config"].get("type") == "s3":
-        try:
+    try:
+        if event["config"].get("type") == "s3":
             o = urlparse(event["config"].get("url"), allow_fragments=False)
 
             s3_client = boto3.client(
@@ -19,44 +19,104 @@ def handler(event, context):
                 aws_secret_access_key=event["config"].get("aws_secret_access_key"),
             )
 
-            s3res = s3_client.get_object(Bucket=o.netloc, Key=o.path.lstrip("/"))
+            key_path = o.path.lstrip("/")
 
-            handler = io.BytesIO(s3res["Body"].read())
+            recursive = False
+            if len(key_path) == 0:
+                recursive = True
+            else:
+                try:
+                    s3_client.head_object(Bucket=o.netloc, Key=o.path.lstrip("/"))
+                except Exception as e:
+                    if "Not Found" in str(e): # If there is no object it is likely a directory
+                        recursive = True
+                    else:
+                        raise e
+
+            if event["config"].get("collection") is None and recursive:
+                raise Exception("Cannot perform a recursive search on a single upload - use a collection")
+
+            if not recursive:
+                s3res = s3_client.get_object(Bucket=o.netloc, Key=o.path.lstrip("/"))
+
+                handler = io.BytesIO(s3res["Body"].read())
+                file = os.path.basename(urlparse(event["config"].get("url")).path)
+                single(event, file, handler)
+            else:
+                # TODO: At the moment this supports up to 1000 keys
+                s3list = s3_client.list_objects_v2(Bucket=o.netloc, Prefix=o.path.lstrip("/"))
+
+                for key in s3list['Contents']:
+                    s3res = s3_client.get_object(Bucket=o.netloc, Key=key["Key"])
+
+                    handler = io.BytesIO(s3res["Body"].read())
+                    file = os.path.basename(urlparse(key["Key"]).path)
+                    single(event, file, handler, collection=event["config"]["collection"])
+
+        elif event["config"].get("type") == "http":
+            res = requests.get(event["config"].get("url"), stream=True)
+            res.raise_for_status()
+
             file = os.path.basename(urlparse(event["config"].get("url")).path)
-        except Exception as e: # TODO Post Error Step
-            raise e
+            handler = ResponseStream(res.iter_content(64))
 
-    elif event["config"].get("type") == "http":
-        res = requests.get(event["config"].get("url"), stream=True)
-        res.raise_for_status()
+            single(event, file, handler)
+        else:
+            print("Unknown Obtain Type")
+            exit()
 
-        file = os.path.basename(urlparse(event["config"].get("url")).path)
-        handler = ResponseStream(res.iter_content(64))
-    else:
-        print("Unknown Obtain Type")
-        exit()
+    except Exception as e: # TODO Post Error Step
+        raise e
 
+
+
+
+
+def single(event, file, handler, collection=None):
+    print(f"Processing: {file}")
     ru_s3 = boto3.client("s3")
+
+    if collection is not None:
+        res = requests.post(
+            f"{os.environ.get('API')}/api/upload",
+            headers={"Authorization": f'bearer {event.get("token")}'},
+            json={
+                "name": file,
+                "uploaded": True,
+                "collection_id": collection
+            },
+        )
+
+        res.raise_for_status()
+        upload = res.json()["id"]
+
+        print(f"Created Upload: {upload} (Collection: {collection})")
+    else:
+        upload = event["config"].get("upload")
 
     ru_s3.put_object(
         Bucket=os.environ["BUCKET"],
-        Key=f'uploads/{event["config"].get("upload")}/{file}',
+        Key=f'uploads/{upload}/{file}',
         Body=handler,
     )
 
     meta = ru_s3.head_object(
         Bucket=os.environ["BUCKET"],
-        Key=f'uploads/{event["config"].get("upload")}/{file}',
+        Key=f'uploads/{upload}/{file}',
     )
 
+    print(f"Patching Upload: {upload}")
     res = requests.patch(
-        f"{os.environ.get('API')}/api/upload/{event['config'].get('upload')}",
+        f"{os.environ.get('API')}/api/upload/{upload}",
         headers={"Authorization": f'bearer {event.get("token")}'},
-        json={"size": meta.get("ContentLength"), "name": file, "uploaded": True},
+        json={
+            "size": meta.get("ContentLength"),
+            "name": file,
+            "uploaded": True
+        },
     )
 
     res.raise_for_status()
-
 
 class ResponseStream(object):
     def __init__(self, request_iterator):
---
+++ tasks/obtain/test/multiple.py
@@ -0,0 +1,73 @@
+import os
+import json
+import boto3
+import unittest
+
+from unittest import mock
+from moto import mock_s3
+from handler import handler
+
+class MockResponse:
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
+
+    def raise_for_status(self):
+        return True
+
+
+def mocked_patch_requests(*args, **kwargs):
+    return MockResponse({
+        "id": 1
+    }, 200)
+
+def mocked_post_requests(*args, **kwargs):
+    return MockResponse({
+        "id": 1
+    }, 200)
+
+class TestSingle(unittest.TestCase):
+
+    @mock_s3
+    @mock.patch('requests.patch', side_effect=mocked_patch_requests)
+    @mock.patch('requests.post', side_effect=mocked_post_requests)
+    def test_single(self, mock_patch, mock_post):
+        os.environ["AWS_ACCESS_KEY_ID"] = '123'
+        os.environ["AWS_SECRET_ACCESS_KEY"] = '123'
+
+        mock_s3 = boto3.resource('s3', region_name='us-east-1')
+        mock_s3.create_bucket(Bucket='export-bucket')
+        mock_s3.create_bucket(Bucket='import-bucket')
+
+        object = mock_s3.Object('import-bucket', 'test.tif')
+        object.put(Body=b'123')
+        object = mock_s3.Object('import-bucket', 'folder/folder2/test2.tif')
+        object.put(Body=b'123')
+        object = mock_s3.Object('import-bucket', 'folder/folder2/test3.gif')
+        object.put(Body=b'123')
+
+        os.environ["BUCKET"] = 'export-bucket'
+        os.environ["API"] = 'http://example.com/api'
+
+        res = handler({
+            "Records": [{
+                "body": json.dumps({
+                    "config": {
+                        "type": "s3",
+                        "collection": 1,
+                        "url": "s3://import-bucket/folder/",
+                        "aws_access_key_id": "key_123",
+                        "aws_secret_access_key": "secret_123"
+                    }
+                })
+            }]
+        }, None)
+
+        self.assertEqual(len(mock_patch.call_args_list), 2)
+        self.assertEqual(len(mock_post.call_args_list), 2)
+
+if __name__ == '__main__':
+    unittest.main()
