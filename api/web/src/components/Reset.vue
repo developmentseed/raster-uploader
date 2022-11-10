312af465df53df5b22f82fc67b0bac3d02801ab9
ingalls <nick@ingalls.ca>
2022-11-10 11:29:16 -0700

    Fix reset password background

--- api/web/src/components/Reset.vue
+++ api/web/src/components/Reset.vue
@@ -1,41 +1,43 @@
 <template>
     <div class='col col--12 grid pt12'>
         <div class='col col--12 flex flex--center-main'>
-            <h3 class='flex-child txt-h4 py6'>Reset Password</h3>
+            <h3 class='flex-child txt-h4 py6 color-white'>Reset Password</h3>
         </div>
 
-        <template v-if='loading'>
-            <Loading/>
-        </template>
-        <template v-if='!success'>
-            <div class='col col--12 flex flex--center-main'>
-                <div class='w240 col col--12 grid grid--gut12'>
-                    <label class='mt12'>Reset Token:</label>
-                    <input v-on:keyup.enter='reset' :class='{
-                         "input--border-red": attempted && !token
-                    }' v-model='token' type='text' class='input'/>
+        <div class='bg-white round px12 py12 col col--12'>
+            <template v-if='loading'>
+                <Loading/>
+            </template>
+            <template v-if='!success'>
+                <div class='col col--12 flex flex--center-main'>
+                    <div class='w240 col col--12 grid grid--gut12'>
+                        <label class='mt12'>Reset Token:</label>
+                        <input v-on:keyup.enter='reset' :class='{
+                             "input--border-red": attempted && !token
+                        }' v-model='token' type='text' class='input'/>
 
-                    <label class='mt12 col col--12'>
-                        New Password:
-                    </label>
-                    <input v-on:keyup.enter='reset' :class='{
-                         "input--border-red": attempted && !password
-                   } ' v-model='password' type='password' class='input'/>
+                        <label class='mt12 col col--12'>
+                            New Password:
+                        </label>
+                        <input v-on:keyup.enter='reset' :class='{
+                             "input--border-red": attempted && !password
+                       } ' v-model='password' type='password' class='input'/>
 
-                    <button @click='reset' class='mt12 w-full color-gray color-green-on-hover btn btn--stroke round'>Reset Password</button>
+                        <button @click='reset' class='mt12 w-full color-gray color-green-on-hover btn btn--stroke round'>Reset Password</button>
+                    </div>
                 </div>
-            </div>
-        </template>
-        <template v-else>
-            <div class='col col--12 flex flex--center-main py24'>
-                <svg class='icon color-green w60 h60'><use href='#icon-check'/></svg>
-            </div>
-            <div class='col col--12 flex flex--center-main'>
-                <div>Password Reset</div>
-            </div>
+            </template>
+            <template v-else>
+                <div class='col col--12 flex flex--center-main py24'>
+                    <svg class='icon color-green w60 h60'><use href='#icon-check'/></svg>
+                </div>
+                <div class='col col--12 flex flex--center-main'>
+                    <div>Password Reset</div>
+                </div>
 
-            <button @click='$router.push("/login")' class='mt12 w-full color-gray color-green-on-hover btn btn--stroke round'>Login</button>
-        </template>
+                <button @click='$router.push("/login")' class='mt12 w-full color-gray color-green-on-hover btn btn--stroke round'>Login</button>
+            </template>
+        </div>
     </div>
 </template>
 
